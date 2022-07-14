92ceae1f97723bcbf04d027e84ab955d44652efa
Pateldeep811 <108999233+Pateldeep811@users.noreply.github.com>
2022-07-15 00:48:45 +0100

    Add files via upload

---
+++ IndianFlag.java
@@ -0,0 +1,36 @@
+package week1;
+
+public class IndianFlag {public static void main(String[] args){
+
+    System.out.println("  **=====================================| ");
+    System.out.println("  **                                     | ");
+    System.out.println("  **                                     | ");
+    System.out.println("  **                                     | ");
+    System.out.println("  **                                     | ");
+    System.out.println("  **                                     | ");
+    System.out.println("  **=====================================| ");
+    System.out.println("  **                 * * *               | ");
+    System.out.println("  **               * * * * *             | ");
+    System.out.println("  **              * * * * * *            | ");
+    System.out.println("  **               * * * * *             | ");
+    System.out.println("  **                 * * *               | ");
+    System.out.println("  **=====================================| ");
+    System.out.println("  **                                     | ");
+    System.out.println("  **                                     | ");
+    System.out.println("  **                                     | ");
+    System.out.println("  **                                     | ");
+    System.out.println("  **                                     | ");
+    System.out.println("  **=====================================| ");
+    System.out.println("  **                                       ");
+    System.out.println("  **                                       ");
+    System.out.println("  **                                       ");
+    System.out.println("  **                                       ");
+    System.out.println("  **                                       ");
+    System.out.println("  **                                       ");
+    System.out.println("  **                                       ");
+    System.out.println("|----|                                      ");
+    System.out.println("|----|                                     ");
+
+
+}
+}
